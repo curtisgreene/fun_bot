5f4383f3049e97fb1dd6455bbe5b3a8043e15d14
curtisgreene <curtis.j.w.greene@gmail.com>
2017-09-07 13:21:37 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  token: "xoxb-237094387075-nVhFyUPOFNavTx0lgzE2jlCz"
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot();
+
+
+// give the bot something to listen for.
+controller.hears([/.*/], ['direct_message','direct_mention','mention'], function(bot,message) {
+  let normalizedMessage = message.text.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "")
+  console.log(normalizedMessage)
+  if (normalizedMessage === 'hello') {
+    bot.reply(message,'Hello yourself.');
+  }
+  else if (normalizedMessage === 'what time is it') {
+    bot.reply(message, `It is ${new Date()}`)
+  }
+  else {
+    bot.reply(message, "I couldn't possible know what you mean.")
+  }
+});
+
+var bot = controller.spawn({
+  token:require('./config').token
+})
+
+bot.startRTM(function(err, bot, payload) {
+  if (err){
+    console.log(err);
+    throw new Error('Could not connect to slack!')
+  }
+})
